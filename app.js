54b0dd2f576339bfdd73abc4e33f71b6ead63a8e
Ainur Dzhaianbaeva <ainurdzhaianbaeva@MacBook-Pro-7.local>
2019-12-04 18:25:54 -0800

    add an image of a dice,practice  using the  querySelector to read and store the text from webpage and also style the element of the webpage (as an example i used querySelector to hide the image of the dice

--- app.js
+++ app.js
@@ -9,3 +9,15 @@ dice=Math.floor(Math.random()*6)+1;
 
  document.querySelector('#current-'+activePlayer).textContent = dice
 // document.querySelector('#current-'+activePlayer).innerHTML="<em>"+dice+"</em>";
+
+
+
+//  We can also read the elements from a webpage using querySelector method and store it in a variable:
+
+var x = document.querySelector('#score-0').textContent;
+console.log(x)
+
+
+//We can also use the querySelector to change the css of element of the webpage
+
+document.querySelector('.dice').style.display ='none';
