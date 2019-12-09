8f0c0bce3a7baaede7ff2cd7e0990324803af264
Ainur Dzhaianbaeva <ainurdzhaianbaeva@MacBook-Pro-7.local>
2019-12-08 21:23:24 -0800

    create an event listener, and function that displays dice randomly as the roll the dice button clicked

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Declaring variables
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 scores=[0,0];
 roundScore=0;
 activePlayer=0;
@@ -21,3 +21,16 @@ console.log(x)
 //We can also use the querySelector to change the css of element of the webpage
 
 document.querySelector('.dice').style.display ='none';
+
+
+document.querySelector('.btn-roll').addEventListener('click', function(){
+  //1. Random Number
+  var dice=Math.floor(Math.random()*6)+1;
+
+  //2. Display the result
+  var diceDOM = document.querySelector('.dice');
+  diceDOM.style.display='block';
+  diceDOM.src='dice-'+dice+'.png';
+
+  //3. Update the round score if the rolled number was not a 1
+})
