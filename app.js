f45e0873943ca616de381070cbfc18247af6ffb9
Ainur Dzhaianbaeva <ainurdzhaianbaeva@MacBook-Pro-7.local>
2020-01-08 18:41:27 -0800

    applies winner styling class and removes active styling class once winner is defined

--- app.js
+++ app.js
@@ -59,6 +59,9 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
   if (scores[activePlayer] >= 20){
     document.querySelector('#name-'+activePlayer).textContent='Winner!'
     document.querySelector('.dice').style.display='none'
+    document.querySelector('.player-'+activePlayer+'-panel').classList.add('winner');
+    document.querySelector('.player-'+activePlayer+'-panel').classList.remove('active');
+
   }else{
     //nextPlayer
     nextPlayer()
