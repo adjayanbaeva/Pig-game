539bc081d543ecee70ea8419be2e982a428d0fba
Ainur Dzhaianbaeva <ainurdzhaianbaeva@MacBook-Pro-7.local>
2020-01-08 19:23:32 -0800

    reset the name back to player-1 and player-2 when new game button is pressed

--- app.js
+++ app.js
@@ -85,4 +85,6 @@ function init(){
   document.getElementById('score-1').textContent='0';
   document.getElementById('current-0').textContent='0';
   document.getElementById('current-1').textContent='0';
+  document.getElementById('name-0').textContent="Player 1";
+  document.getElementById('name-1').textContent="Player 2";
 }
